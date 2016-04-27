c5bd55c4fc1299c073162ff988fac5359d88554e
ducnguyen123 <heocondangyeu0@gmail.com>
2016-04-27 18:49:49 +0700

    Third Commit
    Adjust display

--- Newfolder/js/controllers.js
+++ Newfolder/js/controllers.js
@@ -6,7 +6,7 @@ nameSpace.controller('Extra-info',['$scope','$http', function($scope, $http)
 		{    
 			$http.get('js/profile.json').success(function(res){
 				$scope.profile = res.Test;
-				$scope.Summary = res.Summary;
+				
 			}); 
 
 		}]
