86ab23004e23f29d8d78bbbfc43307952ed19f62
ducnguyen123 <heocondangyeu0@gmail.com>
2016-04-27 21:21:37 +0700

    Four Commit
    Project and images

--- Newfolder/js/controllers.js
+++ Newfolder/js/controllers.js
@@ -1,13 +1,14 @@
 var nameSpace = angular.module("ProfileApp", []);
 
-
-
 nameSpace.controller('Extra-info',['$scope','$http', function($scope, $http)
 		{    
 			$http.get('js/profile.json').success(function(res){
 				$scope.profile = res.Test;
-				
+				$scope.background = res.Background;
+				$scope.info = res.Info;
+				$scope.Exp = res.Experience;
 			}); 
 
 		}]
 );
+
